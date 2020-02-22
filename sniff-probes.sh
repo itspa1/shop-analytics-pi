c9e9432e8a100a4990e88fb66962e070196af092
Pavan Kumar <kumarpavan1309@gmail.com>
2020-02-22 21:24:36 +0530

    made sniff packet max size 65535, removed channel 52 (freezing the wifi)

--- sniff-probes.sh
+++ sniff-probes.sh
@@ -14,7 +14,7 @@ channel_hop() {
 
 	IEEE80211bg="1 2 3 4 5 6 7 8 9 10 11"
 	IEEE80211bg_intl="$IEEE80211b 12 13 14"
-	IEEE80211a="36 40 44 48 52 56 60 64 149 153 157 161"
+	IEEE80211a="36 40 44 48 56 60 64 149 153 157 161"
 	IEEE80211bga="$IEEE80211bg $IEEE80211a"
 	IEEE80211bga_intl="$IEEE80211bg_intl $IEEE80211a"
 
@@ -36,4 +36,4 @@ fi
 
 #use tcpdump to get the probe-req on the interface
 #use -tttt for full timestamp with date format
-sudo tcpdump -tttt -l -I -i "$IFACE" -e -s 256 type mgt subtype probe-req 2> /dev/null
+sudo tcpdump -tttt -l -I -i "$IFACE" -e -s 65535 type mgt subtype probe-req 2> /dev/null
