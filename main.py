b36e474f6122d0bbe2b3ceb78dd37b209fedbfc1
Pavan Kumar <kumarpavan1309@gmail.com>
2020-04-30 12:50:35 +0530

    implemented YOLO model and added it as a module, which supports either yolo/yolo-tiny

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # from wifiBasedModule import initializeWifiModule
 
-import detectionModules.camera.tf
-
+# import detectionModules.camera.tf
+import detectionModules.camera.yolo
 # from detectionModules.wifi import initializeWifiModule
 
 # initializeWifiModule()
